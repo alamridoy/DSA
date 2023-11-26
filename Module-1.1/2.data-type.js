9eb90440c8d05a6ba916b65f4bace7c40060cc0f
alam ridoy <alamridoy7@gmail.com>
2023-11-26 21:37:15 +0600

    class-2 obj/array done

--- Module-1.1/2.data-type.js
+++ Module-1.1/2.data-type.js
@@ -1,12 +1,38 @@
 // var , let , const
 
 let a = null
-console.log(typeof a)
+//console.log(typeof a)
 
 let b = undefined
 
-console.log(typeof b)
+//console.log(typeof b)
 
 let c = NaN
-console.log(c)
+//console.log(c)
 
+
+let obj = {
+    name:"Ridoy",
+    age:24
+}
+obj.address = "dhaka"
+obj.university = "DIU"
+
+
+console.log(obj['age'])
+
+
+
+let arr = ["ridsoyt",24,{sdd:"dsfd"},[33,445,666]]
+
+// index in index
+
+console.log(arr[3][3])
+
+console.log(arr.length-1) // last element index
+
+
+let arr2 = [1,2,3,4,5]
+
+console.log(arr2[arr2.length-1])
+console.log(arr2.length-1)
