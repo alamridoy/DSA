dd21b92bf06d6a79d38d099fd5ffc2ffa1b41898
alam ridoy <alamridoy7@gmail.com>
2023-11-27 22:02:45 +0600

    class-4 conditing nasted loop

---
+++ Module-1.1/4.conditional&function.js
@@ -0,0 +1,33 @@
+
+
+let num = 30
+
+let res = num>20 ? "Hello":"sdd"
+console.log(res)
+
+
+function add(num1,num2){
+    let res = (num1+num2) /2
+    return res
+}
+
+console.log(add(10,20))
+console.log(add(30,60))
+
+
+
+
+for (let i = 0; i < 3; i++) {
+   
+
+    for (let j = 0; j < 3; j++) {
+       
+        console.log(i,j)
+       
+        
+    }
+ 
+
+    console.log(i)
+    
+}
