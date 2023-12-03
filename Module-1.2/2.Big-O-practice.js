fd4b016a1fd46b3d9a9a57abeabdf8d7823d0e76
alam ridoy <alamridoy7@gmail.com>
2023-12-04 00:24:53 +0600

    1.2 => 2 done

--- Module-1.2/2.Big-O-practice.js
+++ Module-1.2/2.Big-O-practice.js
@@ -26,3 +26,15 @@ function logAtLeastMin(n){
     //   logAtLeastMin(10) // ==> O(1) if grather than 5 it is best case
 
 
+
+
+    // Big-O steps  best to wrost
+
+    //1.    O(1)
+    //2.    O(log n)
+    //3.    O(n)
+    //4.    O(n log n)
+    //5.   O(n^2)
+    //6.   O(2^n)
+
+
