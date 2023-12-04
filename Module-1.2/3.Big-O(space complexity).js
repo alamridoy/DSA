4440a2c8fe3690fc88e425ad2c5d1f5b77c66fbc
alam ridoy <alamridoy7@gmail.com>
2023-12-04 21:40:02 +0600

    3.Big-O(space complexity).js done

---
+++ Module-1.2/3.Big-O(space complexity).js
@@ -0,0 +1,15 @@
+// space complexity 
+// Big O
+
+
+// ==> most primitives (booleans,numbers,undefine,null) are constant space
+// ex: const number = 100   -- O(1)
+// but strig is linear const name = 'ridot' O(n)
+
+
+
+//==> and non premetive (array,object) same as define as liner = O(n)
+// ex: const arr = [1,2,3,4,5]   -- O(n)  if arr length increase space comlpx is increase
+// ex: const ibj = {"name":"Ridoy","name":"hasan"}   --  O(n) key
+// ex: const str = 'Ridoy   -- O(n)  str
+
