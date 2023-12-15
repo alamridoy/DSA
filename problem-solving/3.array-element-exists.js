d099f5666f9d508e6d24ee86ae2a1e2b3eab3db7
alam ridoy <alamridoy7@gmail.com>
2023-12-16 01:37:36 +0600

    problem-solving 3.array-element-exists solve 2 way

---
+++ problem-solving/3.array-element-exists.js
@@ -0,0 +1,45 @@
+// compare 2 array have match value
+
+
+// function isElementExists(arr1,arr2){
+    
+//     for (let index = 0; index < arr1.length; index++) {
+//         const elementarr1 = arr1[index];
+
+//         for (let i = 0; i < arr2.length; i++) {
+//             const elementarr2 = arr2[i];
+
+//             if(elementarr1 === elementarr2){
+//                 return true
+//             }
+            
+//         }
+       
+        
+//     }
+//     return false
+ 
+// }
+// console.log(isElementExists(['a','b','c'],[1,2,3,'z','c']))
+
+
+
+function isElementExists(arr1,arr2){
+    
+    for (let index = 0; index < arr1.length; index++) {
+        const elementarr1 = arr1[index];
+
+        if(arr2.includes(elementarr1)){
+            return true
+        }
+        
+    }
+
+    return false
+    
+
+   
+ 
+}
+console.log(isElementExists([1,2,3,'a','c'],[4,8,7,0,'a']))
+
