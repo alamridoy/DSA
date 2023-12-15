8267315c135fbbe998d6195c950765c0b733e1b0
alam ridoy <alamridoy7@gmail.com>
2023-12-16 00:40:22 +0600

    problem-solving 2.strObj-count done

---
+++ problem-solving/2.strObj-count.js
@@ -0,0 +1,24 @@
+// user give input some string. this problem solve this string count how many character and count
+
+function StringCount(str){
+    let strObj = {}
+
+    let strNormalized = str.toLowerCase()
+
+    for (let index = 0; index < strNormalized.length; index++) {
+        const strData = strNormalized[index];
+        
+        if(strData === ' ') continue
+        if(strData in strObj){
+            console.log(strData)
+            strObj[strData] += 1
+        }else{
+            strObj[strData] = 1
+        }      
+        
+    }
+
+  return strObj
+}
+
+console.log(StringCount('he lloi i'))
