d9803cd30eedadf9f6c5336f5b7ca00cd32ffc9c
alam ridoy <alamridoy7@gmail.com>
2023-12-16 23:57:47 +0600

    problem-solving 1. basic-problem 1. added new problem solve

--- problem-solving/1.basic-problems.js
+++ problem-solving/1.basic-problems.js
@@ -83,7 +83,7 @@ console.log(removeDuplicate([1, 2, 3, 4, 2, 4, 5, 6, 1,1]));
 
 
 // Question : here two array num1 = [1,2,3,4,5] and num2 = [1,2,3,1,2,1,3,4,5,2,3,4,1]. out this answer how many time first array number have second number
-
+// time complexity O(n+n) => O(n)
 function countArray(num1, num2) {
     let trackCountArray = {};
   
@@ -109,3 +109,26 @@ function countArray(num1, num2) {
 
 
 
+  // Q: Write a program that prints the numbers from 1 to 100 . But for multiple of three print "Fizz" instead of the number and for the multiple of five print "Buzz".FOr numbers which are multiples of both and five print "FIzzBuzz"
+
+  function calculate(){
+  let obj = {}
+    for (let index = 1; index <= 60; index++) {
+        const element = index
+        obj.element = element
+
+        if(element % 3 == 0 && element % 5 == 0){
+            obj.element = "FizzBuzz"
+        }else if(element % 3 == 0) {
+           obj.element = "Fizz"
+          
+        }else if(element % 5 == 0){
+            obj.element = "Buzz"
+        }
+        console.log(obj) 
+    }
+  
+
+  }
+
+  calculate()
